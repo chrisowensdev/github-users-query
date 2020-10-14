d9794f96f12bb711ae0934915707a708b2ed7bd4
Chris Owens <guitarrockstarco@gmail.com>
2020-10-14 14:50:28 -0400

    Update user profile to display name and login

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -3,28 +3,33 @@ import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 
 const UserProfile = props => {
+    const { users } = props;
     const [ repos, setRepos ] = useState([])
-
     const { username } = useParams();
-    console.log("Params", username);
 
     useEffect(() => {
         (async function() {
-            const response = await fetch(`https://api.github.com/users/${username}/repos`);
+            const response = await fetch(`https://api.github.com/users/${username}/repos?page=1&per_page=200`);
             const repos = await response.json();
             setRepos(repos);
         })();
     }, [setRepos, username]);
-
-
+    
+    const user = users.find((user) => {
+        return user.login === username ? user : null;
+    })
+    console.log(user);
     return (
         <>
+        <Link to="/">Return to the search</Link>
+        <h1>{user.name}</h1>
+        <h3>{user.login}</h3>
         <ul>
         {repos.map(repo => {
         return (<li key={repo.id}><a href={repo.html_url}>{repo.name}</a></li>);      
     })}
         </ul>
-        <Link to="/">Return to the search</Link>
+        
         </>
     )
 }
